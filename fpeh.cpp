8acf50dceb14ab6c989d2eae0bcc43b1867604ef
scivision <scivision@users.noreply.github.com>
2023-07-28 08:16:03 -0400

    add c++ demo

--- fpeh.cpp
+++ fpeh.cpp
@@ -5,46 +5,44 @@
 
 #pragma STDC FENV_ACCESS ON
 
-#include <signal.h>      // for signal()
-#include <fenv.h>       // for fegetenv(), fesetenv()
-#include <math.h>
-#include <float.h>
+#include <csignal>      // for signal()
+#include <cfenv>       // for fegetenv(), fesetenv()
+#include <limits>
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
 
-/* Needed for print_hexval, below */
-#include <string.h>    /* Needed for memcpy */
-#include <inttypes.h>  /* Needed for PRIx64 */
+#include <cstring> // for memcpy()
+#include <cinttypes> // for PRIx64
 
 
 void fpe_signal_handler(int sig) {
-    fprintf(stderr, "Floating point exception encountered\n");
+    std::cerr << "Floating point exception encountered\n";
     /* would like non-zero exit code, but that interferes with CTest */
     switch (sig)
     {
         case FE_INVALID:
-            fprintf(stderr, "Invalid value detected\n");
+            std::cerr << "Invalid value detected\n";
             exit(77);
             break;
         case FE_DIVBYZERO:
-            fprintf(stderr, "Division by zero detected\n");
+            std::cerr << "Division by zero detected\n";
             exit(77);
             break;
         case FE_OVERFLOW:
-            fprintf(stderr, "Overflow detected\n");
+            std::cerr << "Overflow detected\n";
             exit(77);
             break;
         case FE_UNDERFLOW:
-            fprintf(stderr, "Underflow detected\n");
+            std::cerr << "Underflow detected\n";
             exit(77);
             break;
         case FE_INEXACT:
-            fprintf(stderr, "Inexact detected\n");
+            std::cerr << "Inexact detected\n";
             exit(77);
             break;
         default :
-            fprintf(stderr, "Unknown exception detected\n");
+            std::cerr << "Unknown exception detected\n";
             exit(77);
             break;
     }
@@ -57,21 +55,21 @@ void enable_floating_point_exceptions()
  feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW | FE_INEXACT);
 #else /* macOS */
  fenv_t env;
- if(fegetenv(&env)){
-    fprintf(stderr, "fegetenv() failed\n");
+ if(std::fegetenv(&env)){
+    std::cerr << "fegetenv() failed\n";
     exit(EXIT_FAILURE);
  }
 
  env.__fpcr = env.__fpcr | FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW | FE_INEXACT;
- printf("set env var __fpcr to %llx\n", env.__fpcr);
+ std::cout << "set env var __fpcr to " << env.__fpcr  << "\n";
 
- if(fesetenv(&env)){
-    fprintf(stderr, "fesetenv() failed\n");
+ if(std::fesetenv(&env)){
+    std::cerr << "fesetenv() failed\n";
     exit(EXIT_FAILURE);
  }
 #endif
 
- signal(SIGFPE, fpe_signal_handler);
+ std::signal(SIGFPE, fpe_signal_handler);
 }
 
 
@@ -103,6 +101,7 @@ void print_hexval(double x)
     printf("x = %16f (%" PRIx64 ")\n", x, xn);
 }
 
+
 static
 double create_exception(int choice)
 {
@@ -111,49 +110,49 @@ double create_exception(int choice)
     {
         case 1:
         x = -1;
-        y = sqrt(x);  // Or acos(x-1), log(x)
-        printf("1: INVALID :");
+        y = std::sqrt(x);  // Or acos(x-1), log(x)
+        std::cout << "1: INVALID :";
         print_hexval(y);
         break;
 
         case 2:
         x = 100000;
-        y = exp(x);
-        printf("2: OVERFLOW :");
+        y = std::exp(x);
+        std::cout << "2: OVERFLOW :";
         print_hexval(y);
         break;
 
         case 3:
-        y = DBL_MIN / 10;
-        printf("3: UNDERFLOW :");
+        y = std::numeric_limits<double>::min() / 10;
+        std::cout << "3: UNDERFLOW :";
         print_hexval(y);
         break;
 
         case 4:
         x = 0;
         y = 1/x;
-        printf("4: DIVBYZERO :");
+        std::cout << "4: DIVBYZERO :";
         print_hexval(y);
         break;
 
         case 5:
         set_qnan(&x);
-        printf("5: Quiet NAN (not trapped) :");
+        std::cout << "5: Quiet NAN (not trapped) :";
         print_hexval(x);
         y = x + 1;
         break;
 
         case 6:
         set_snan(&x);
-        printf("6: Signaling NAN (trapped) :");
+        std::cout << "6: Signaling NAN (trapped) :";
         print_hexval(x);
         y = x + 1;
         break;
 
         default:
         y = -1;
-        printf("Usage : tst_fpe <fpe_id>\n");
-        printf("        fpe_id in [1-5]\n\n");
+        std::cout << "Usage : tst_fpe <fpe_id>\n";
+        std::cout << "        fpe_id in [1-5]\n\n";
         exit(EXIT_FAILURE);
     }
     return y;
@@ -162,14 +161,14 @@ double create_exception(int choice)
 void show_fe_exceptions(void)
 {
     // from https://en.cppreference.com/w/c/numeric/fenv/feenv
-    printf("current exceptions raised: ");
-    if(fetestexcept(FE_DIVBYZERO))     printf(" FE_DIVBYZERO");
-    if(fetestexcept(FE_INEXACT))       printf(" FE_INEXACT");
-    if(fetestexcept(FE_INVALID))       printf(" FE_INVALID");
-    if(fetestexcept(FE_OVERFLOW))      printf(" FE_OVERFLOW");
-    if(fetestexcept(FE_UNDERFLOW))     printf(" FE_UNDERFLOW");
-    if(fetestexcept(FE_ALL_EXCEPT)==0) printf(" none");
-    printf("\n");
+    std::cout << "current exceptions raised: ";
+    if(std::fetestexcept(FE_DIVBYZERO))     std::cout << " FE_DIVBYZERO";
+    if(std::fetestexcept(FE_INEXACT))       std::cout << " FE_INEXACT";
+    if(std::fetestexcept(FE_INVALID))       std::cout << " FE_INVALID";
+    if(std::fetestexcept(FE_OVERFLOW))      std::cout << " FE_OVERFLOW";
+    if(std::fetestexcept(FE_UNDERFLOW))     std::cout << " FE_UNDERFLOW";
+    if(std::fetestexcept(FE_ALL_EXCEPT)==0) std::cout << " none";
+    std::cout << "\n";
 }
 
 
@@ -179,12 +178,12 @@ int main(int argc, char** argv)
     int use_eh = 0;
 
     if (argc > 1)
-        fpe_id = atoi(argv[1]);
+        fpe_id = std::atoi(argv[1]);
     if (argc > 2)
-        use_eh = atoi(argv[2]);
+        use_eh = std::atoi(argv[2]);
 
     if (use_eh){
-        printf("Enabling exception handling\n");
+        std::cout << "Enabling exception handling\n";
         enable_floating_point_exceptions();
     }
 
@@ -192,7 +191,7 @@ int main(int argc, char** argv)
     double y = create_exception(fpe_id);
     show_fe_exceptions();
 
-    printf("Result : y = %g\n",y);
+    std::cout << "Result : y = " << y << "\n";
 
     return EXIT_SUCCESS;
 }
