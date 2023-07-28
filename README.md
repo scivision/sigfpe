04f7cff5645404fccaf3e6584e330b202a94365e
scivision <scivision@users.noreply.github.com>
2023-07-28 07:53:11 -0400

    gist to repo

---
+++ README.md
@@ -0,0 +1,37 @@
+# Apple Silicon exception handling
+
+[Original idea](https://github.com/ForestClaw/forestclaw/pull/201/files)
+
+This example implements DYI
+[floating point](https://en.cppreference.com/w/c/numeric/fenv)
+exception handling.
+
+This approach is for Unix-like operating systems only at this time (Linux, macOS, ...).
+Windows could be a [future extension](https://learn.microsoft.com/en-us/previous-versions/visualstudio/visual-studio-2012/e9b52ceh(v=vs.110)).
+
+It can be tested with
+
+```sh
+test_fpe <fpe_id>
+```
+
+where <fpe_id> is an FPE type to trap.
+
+Valid types are indicated by integer 1-5 :
+
+1 : INVALID, e.g. sqrt(-1), log(-1), acos(2) etc.
+
+2 : OVERFLOW, e.g. 2^1024, exp(1000)
+
+3 : UNDERFLOW, e.g. denormalized numbers such as 2^(-1074), exp(-710)
+
+4 : DIVBYZERO, e.g. 1/0
+
+5 : Quiet NAN : This is useful for detecting use of uninitialized data
+
+6 : Signaling NAN : This NAN will get trapped.
+
+Ideas for code:
+
+* https://developer.apple.com/forums/thread/689159
+* https://stackoverflow.com/questions/69059981/how-to-trap-floating-point-exceptions-on-m1-macs
---
+++ fpeh.c
@@ -0,0 +1,200 @@
+// https://en.cppreference.com/w/c/numeric/fenv
+
+#define _GNU_SOURCE 1
+/* before headers, necessary on Linux */
+
+#pragma STDC FENV_ACCESS ON
+
+#include <signal.h>      // for signal()
+#include <fenv.h>       // for fegetenv(), fesetenv()
+#include <math.h>
+#include <float.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Needed for print_hexval, below */
+#include <string.h>    /* Needed for memcpy */
+#include <inttypes.h>  /* Needed for PRIx64 */
+
+
+void fpe_signal_handler(int sig) {
+    fprintf(stderr, "Floating point exception encountered\n");
+    /* would like non-zero exit code, but that interferes with CTest */
+    switch (sig)
+    {
+        case FE_INVALID:
+            fprintf(stderr, "Invalid value detected\n");
+            exit(77);
+            break;
+        case FE_DIVBYZERO:
+            fprintf(stderr, "Division by zero detected\n");
+            exit(77);
+            break;
+        case FE_OVERFLOW:
+            fprintf(stderr, "Overflow detected\n");
+            exit(77);
+            break;
+        case FE_UNDERFLOW:
+            fprintf(stderr, "Underflow detected\n");
+            exit(77);
+            break;
+        case FE_INEXACT:
+            fprintf(stderr, "Inexact detected\n");
+            exit(77);
+            break;
+        default :
+            fprintf(stderr, "Unknown exception detected\n");
+            exit(77);
+            break;
+    }
+}
+
+void enable_floating_point_exceptions()
+{
+
+#ifdef HAVE_FEENABLEEXCEPT /* Linux */
+ feenableexcept(FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW | FE_INEXACT);
+#else /* macOS */
+ fenv_t env;
+ if(fegetenv(&env)){
+    fprintf(stderr, "fegetenv() failed\n");
+    exit(EXIT_FAILURE);
+ }
+
+ env.__fpcr = env.__fpcr | FE_INVALID | FE_DIVBYZERO | FE_OVERFLOW | FE_UNDERFLOW | FE_INEXACT;
+ printf("set env var __fpcr to %llx\n", env.__fpcr);
+
+ if(fesetenv(&env)){
+    fprintf(stderr, "fesetenv() failed\n");
+    exit(EXIT_FAILURE);
+ }
+#endif
+
+ signal(SIGFPE, fpe_signal_handler);
+}
+
+
+static
+void set_qnan(double *f)
+{
+    /*
+     The quiet nan from math.h
+    */
+    *f = NAN;
+}
+
+static
+void set_snan(double* f)
+{
+    /* From :
+      "NaNs, Uninitialized Variables, and C++"
+      http://codingcastles.blogspot.fr/2008/12/nans-in-c.html
+    */
+    *((long long*)f) = 0x7ff0000000000001LL;
+}
+
+static
+void print_hexval(double x)
+{
+    /* Kind of cool - didn't know it was possible to do this */
+    uint64_t xn;
+    memcpy(&xn, &x, sizeof x);
+    printf("x = %16f (%" PRIx64 ")\n", x, xn);
+}
+
+static
+double create_exception(int choice)
+{
+    double x,y;
+    switch(choice)
+    {
+        case 1:
+        x = -1;
+        y = sqrt(x);  // Or acos(x-1), log(x)
+        printf("1: INVALID :");
+        print_hexval(y);
+        break;
+
+        case 2:
+        x = 100000;
+        y = exp(x);
+        printf("2: OVERFLOW :");
+        print_hexval(y);
+        break;
+
+        case 3:
+        y = DBL_MIN / 10;
+        printf("3: UNDERFLOW :");
+        print_hexval(y);
+        break;
+
+        case 4:
+        x = 0;
+        y = 1/x;
+        printf("4: DIVBYZERO :");
+        print_hexval(y);
+        break;
+
+        case 5:
+        set_qnan(&x);
+        printf("5: Quiet NAN (not trapped) :");
+        print_hexval(x);
+        y = x + 1;
+        break;
+
+        case 6:
+        set_snan(&x);
+        printf("6: Signaling NAN (trapped) :");
+        print_hexval(x);
+        y = x + 1;
+        break;
+
+        default:
+        y = -1;
+        printf("Usage : tst_fpe <fpe_id>\n");
+        printf("        fpe_id in [1-5]\n\n");
+        exit(EXIT_FAILURE);
+    }
+    return y;
+}
+
+void show_fe_exceptions(void)
+{
+#ifdef HAVE_FETESTEXCEPT
+    // from https://en.cppreference.com/w/c/numeric/fenv/feenv
+    printf("current exceptions raised: ");
+    if(fetestexcept(FE_DIVBYZERO))     printf(" FE_DIVBYZERO");
+    if(fetestexcept(FE_INEXACT))       printf(" FE_INEXACT");
+    if(fetestexcept(FE_INVALID))       printf(" FE_INVALID");
+    if(fetestexcept(FE_OVERFLOW))      printf(" FE_OVERFLOW");
+    if(fetestexcept(FE_UNDERFLOW))     printf(" FE_UNDERFLOW");
+    if(fetestexcept(FE_ALL_EXCEPT)==0) printf(" none");
+    printf("\n");
+#endif
+}
+
+
+int main(int argc, char** argv)
+{
+    int fpe_id = 1;
+    int use_eh = 0;
+
+    if (argc > 1)
+        fpe_id = atoi(argv[1]);
+    if (argc > 2)
+        use_eh = atoi(argv[2]);
+
+    if (use_eh){
+        printf("Enabling exception handling\n");
+        enable_floating_point_exceptions();
+    }
+
+    show_fe_exceptions();
+    double y = create_exception(fpe_id);
+    show_fe_exceptions();
+
+    printf("Result : y = %g\n",y);
+
+    return EXIT_SUCCESS;
+}
